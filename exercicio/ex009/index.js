a03f449d95575e484e97e5666393498f79edb991
Your Name <you@example.com>
2022-11-24 13:41:06 -0300

    criando em JS

---
+++ exercicio/ex009/index.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel}KM/h`)
+if (vel >60){ //condição simples
+    console.log('Voce Ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Diriga sempre usano cinto de seguraça')
